c31bf5a9f4f925e0a31836f7cfc5af0438edf2ef
Hideyuki TAKEUCHI <webmaster@chimera.st>
2011-03-04 01:03:34 +0900

    プロジェクト名が間違っていたのを修正

--- project/build/GarbageProject.scala
+++ project/build/GarbageProject.scala
@@ -1,6 +1,6 @@
 import sbt._
 
-class ScalaTanDashProject(info: ProjectInfo) extends DefaultProject(info) {
+class ScalaGarbageProject(info: ProjectInfo) extends DefaultProject(info) {
   override def fork = Some(new ForkScalaRun {
     override def runJVMOptions =
       super.runJVMOptions ++ Seq("-server", "-Xms512m", "-Xmx512m")
